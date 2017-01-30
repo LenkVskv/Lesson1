1714d41e94414592b65c1d89521b53f02dd41ecb
LenkVskv <olenkavaskiv4215@gmail.com>
2017-01-30 11:56:08 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,8 +3,10 @@
  */
 public class Main {
     public static void main( String args[]){
-        Rectangle rectangle = new Rectangle();
+        Rectangle rectangle = new Rectangle(0,0);
         Rectangles rectangles = new Rectangles();
+        System.out.println("Perimetr - "+ rectangle.getPerimetr());
+        System.out.println("Area - "+ rectangle.getArea());
     System.out.println("The total  area of rectansles: "+ rectangle.getArea());
     }
 
